0d6e455381f529944c86e2dde192ca2454b28a49
Camille Caudrillier <camille.caudrillier>
2020-10-05 17:14:30 +0200

    Camille's code

--- ex1.py
+++ ex1.py
@@ -5,8 +5,11 @@
 # Function: function_name
 # ------------------------------------------------------------------------------
 # Function that reads a file containing a crossword and stores it in a matrix
-#
-#   returns: the matrix representation of the crossword
+#   
+#   returns:
+#       crossword = the matrix representation of the crossword
+#       i = vertical size of the crossword
+#       j = horizontal size of the crossword
 #
 def readCrossword():
     file = open("crossword_CB_v2.txt", "r") # Open the file
@@ -51,6 +54,7 @@ def readDictionary():
             dictionary[len(word)-2] = [word] # Add new list with first word
         else:
             dictionary[len(word)-2].append(word) # Append word to list
+    return dictionary
 
 
 # Function: horizontalBeginning
@@ -166,10 +170,156 @@ def calculateSlots(crossword, n, m):
 # ------------------------------------------------------------------------------
 # Executes the main program
 #
-def main():
-    crossword, n, m = readCrossword()
-    slots = calculateSlots(crossword, n, m)
-    dictionary = readDictionary()
+#def main():
+#    crossword, n, m = readCrossword()
+#    slots = calculateSlots(crossword, n, m)
+#    dictionary = readDictionary()
 
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
+
+
+
+# Function: max_word
+# ------------------------------------------------------------------------------
+# Find the first word with maximal length in the crossword
+#
+#   slots: list with the slots' information
+#   m: vertical size of the crossword
+#   n: horizontal size of the crossword
+#
+#   returns: word with maximal length (if there is many, returns the first of the slots' list)
+#       => ['V'/'H' , row, column, length]
+#
+def max_word(slots, m, n):
+    i=0     # i : row index in the slots' list
+    max=0   # maximum word length founded in the crossword
+    while i < len(slots):
+        if(slots[i][3]>max):    # slots[i][3] = length
+            max=slots[i][3]
+            position=slots[i]     
+    return position
+
+
+
+# Function: find_constraint
+# ------------------------------------------------------------------------------
+# Finds if there is any constraint for a word
+# (a constraint is letter already founded whiwh has to be in this word)
+#
+#   word : given word in the slots' list
+#   solution : partial solution of the crossword
+#
+#   constraint_list : list with all constraints of letters for one slot
+#       => [position, letter]
+#
+def find_constraint(word,solution):
+    i=word[1]
+    j=word[2]
+    length=word[3]
+    constraint_list = []  
+    if word[0]=='V':
+        while i <= word[1]+length:
+            if solution[i][j] != '0':
+                letter = solution[i][j]
+                constraint_list = constraint_list + [i,letter]
+                i+=1
+            else:
+                i+=1
+    else :      # if word[0]=='H'       
+        while j <= word[2]+length:
+            if solution[i][j] != '0':
+                letter = solution[i][j]
+                constraint_list = constraint_list + [j,letter]
+                j+=1
+            else:
+                j+=1   
+    return constraint_list
+
+
+
+# Function: find_word
+# ------------------------------------------------------------------------------
+# Searchs in the dictionary if there is a word with a length and constraints
+#
+#   length: length of the searched word
+#   constraint_list: letter constraint in the searched word
+#   dictionary: result of the function 'readDictionary'
+#
+#   returns 0 if there isn't such a word in the dictionary
+#   returns a solution for the searched word if it exists
+#
+def find_word(length, constraint_list, dictionary):
+    word=' '
+    for word in dictionary :
+        if len(word)==length:
+            i=0
+            while i<=len(constraint_list):
+                letter = constraint_list[i][1]
+                position = constraint_list[i][0]
+                if word[position] != letter:
+                    break
+                else:
+                    i+=1
+    if word==' ':
+        return 0
+    else :
+        return word
+
+
+
+# Function: crossword_solution
+# ------------------------------------------------------------------------------
+# Gives a final solution for the given crossword
+#
+#   slots: list with the slots' information
+#   m: vertical size of the crossword
+#   n: horizontal size of the crossword
+#   dictionary: given dictionary
+#   crossword: given crossword
+#
+#   returns a crossword as final solution
+#
+def crossword_solution(slots, m, n, dictionary, crossword):
+    temp_solution = crossword
+    slots_copy = slots
+    # we will erase each word of slots_copy when we find its solution in the dictionary
+    k=0
+
+    while k <= len(slots):
+        word = max_word(slots_copy, m, n)
+        constraint_list = find_constraint(word, temp_solution)
+
+        # backtracking (we search another word if this one doesn't work) :
+        while find_word(word[3], constraint_list, dictionary) == 0:
+            slots_copy_2 = slots_copy
+            slots_copy_2.remove(word)
+            word = max_word(slots_copy_2, m, n)
+
+        # when a word works, we write it in the solution
+        if find_word(word[3], constraint_list, dictionary) != 0:
+            word_from_dictionary = find_word(word[3], constraint_list, dictionary)
+            slots_copy.remove(word)
+
+            i=word[1]
+            j=word[2]
+            length=word[3]
+                
+            if word[0]=='V':
+                while i <= word[1]+length:
+                    temp_solution[i][j] = word_from_dictionary[i]
+                    i+=1
+            else :      # if word[0]='H'       
+                while j <= word[2]+length:
+                    temp_solution[i][j] = word_from_dictionary[j]
+                    j+=1   
+
+        # else :
+        #   is it possible ?
+
+        k+=1
+    
+    final_solution = temp_solution
+    return final_solution
+        
+
